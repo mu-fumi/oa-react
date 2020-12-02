ac2df8358b27932799794bb62495efd9ed32ff60
mu-fumi <1017182993@qq.com>
2020-12-02 15:03:04 +0000

    修改密码

--- src/components/GlobalHeader/UpdatePassWord.tsx
+++ src/components/GlobalHeader/UpdatePassWord.tsx
@@ -1,23 +1,51 @@
 import React, { useState } from 'react';
-import { Modal, Form, Input } from 'antd';
+import { Modal, Form, Input, message } from 'antd';
+import request from '@/utils/request';
+import { ls } from '@/utils/utils';
+import { history } from 'umi';
+
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 6 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 18 },
+  },
+};
+
+export interface UpdatePwdType {
+  old_pwd: string;
+  new_pwd: string;
+  repeatNew_pwd: string;
+}
 
 export default function UpdatePassWord(props: any) {
-  const { visible, showModal, hideModal, currentUser } = props;
+  const { visible, hideModal, currentUser } = props;
+  console.log('currentUser -> :', currentUser);
 
   const [form] = Form.useForm();
 
-  const validatePass2 = (rule: any, value: any, callback: any) => {
-    console.log('value -> :', value)
-    console.log('rule -> :', rule);
-    // if (value !== this.pwd.new_pwd) {
-    //   callback(new Error('两次密码不一样'));
-    // } else {
-    //   callback();
-    // }
-  };
-
   const subForm = () => {
-    form.validateFields();
+    form
+      .validateFields()
+      .then((values: any) => {
+        request('/user/update_pwd', {
+          method: 'POST',
+          data: values,
+        }).then((res: any) => {
+          hideModal();
+          message.success(res.msg + '，请重新登陆');
+          setTimeout(() => {
+            ls.cl();
+            history.push('/users');
+          }, 1400);
+        });
+      })
+      .catch((errorInfo: any) => {
+        console.log('errorInfo -> :', errorInfo);
+      });
   };
 
   return (
@@ -31,14 +59,14 @@ export default function UpdatePassWord(props: any) {
       okText="确认"
       cancelText="取消"
     >
-      <Form form={form}>
-        <Form.Item label="手机号/账号:">{currentUser.username}</Form.Item>
+      <Form preserve={false} form={form} {...formItemLayout}>
+        <Form.Item label="手机号/账号:">{currentUser.mobile}</Form.Item>
         <Form.Item
           label="原密码"
           name="old_pwd"
           rules={[{ required: true, message: '请输入原密码' }]}
         >
-          <Input></Input>
+          <Input placeholder="请输入原密码" />
         </Form.Item>
         <Form.Item
           label="新密码"
@@ -57,7 +85,7 @@ export default function UpdatePassWord(props: any) {
             },
           ]}
         >
-          <Input></Input>
+          <Input placeholder="请输入新密码" />
         </Form.Item>
         <Form.Item
           label="重复新密码"
@@ -66,12 +94,19 @@ export default function UpdatePassWord(props: any) {
             {
               required: true,
               message: '请重复新密码',
-              validateTrigger: 'blur',
             },
-            { validator: validatePass2, validateTrigger: 'change' },
+
+            ({ getFieldValue }) => ({
+              validator(rule, value) {
+                if (!value || getFieldValue('new_pwd') === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject('两次密码不一样');
+              },
+            }),
           ]}
         >
-          <Input></Input>
+          <Input placeholder="请重复新密码" />
         </Form.Item>
       </Form>
     </Modal>
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -7,51 +7,49 @@ export const isUrl = (path: string): boolean => reg.test(path);
 
 export const getPageQuery = () => parse(window.location.href.split('?')[1]);
 
-
-
 /**
  * localStorage 封装
  */
 export const ls = {
-    set: (key: string, data: any) => {
-        if (typeof data === 'object') {
-            data = JSON.stringify(data)
-        }
-        window.localStorage.setItem(key, data)
-    },
-    get: (key: string) => {
-        try {
-            return JSON.parse(window.localStorage.getItem(key) as string)
-        } catch {
-            return window.localStorage.getItem(key)
-        }
-    },
-    cl: (key: string) => {
-        key ? window.localStorage.removeItem(key) : window.localStorage.clear()
+  set: (key: string, data: any) => {
+    if (typeof data === 'object') {
+      data = JSON.stringify(data);
     }
-}
+    window.localStorage.setItem(key, data);
+  },
+  get: (key: string) => {
+    try {
+      return JSON.parse(window.localStorage.getItem(key) as string);
+    } catch {
+      return window.localStorage.getItem(key);
+    }
+  },
+  cl: (key?: string) => {
+    key ? window.localStorage.removeItem(key) : window.localStorage.clear();
+  },
+};
 
 /**
  * sessionstorage 封装
  */
 export const ss = {
-    set: (key: string, data: any) => {
-        if (typeof data === 'object') {
-            data = JSON.stringify(data)
-        }
-        window.sessionStorage.setItem(key, data)
-    },
-    get: (key: string) => {
-        try {
-            return JSON.parse(window.sessionStorage.getItem(key) as string)
-        } catch {
-            return window.sessionStorage.getItem(key)
-        }
-    },
-    cl: (key: string) => {
-        key ? window.sessionStorage.removeItem(key) : window.sessionStorage.clear()
+  set: (key: string, data: any) => {
+    if (typeof data === 'object') {
+      data = JSON.stringify(data);
     }
-}
+    window.sessionStorage.setItem(key, data);
+  },
+  get: (key: string) => {
+    try {
+      return JSON.parse(window.sessionStorage.getItem(key) as string);
+    } catch {
+      return window.sessionStorage.getItem(key);
+    }
+  },
+  cl: (key: string) => {
+    key ? window.sessionStorage.removeItem(key) : window.sessionStorage.clear();
+  },
+};
 
 /**
  *
@@ -60,8 +58,7 @@ export const ss = {
  * @param {*} link  关联的字段名字  pid 。。。
  */
 export function nest(items: any[], id = null, link = 'parent_id'): any {
-    return items
-        .filter(item => item[link] === id)
-        .map(item => ({ ...item, children: nest(items, item.dept_id, link) }))
+  return items
+    .filter(item => item[link] === id)
+    .map(item => ({ ...item, children: nest(items, item.dept_id, link) }));
 }
-
